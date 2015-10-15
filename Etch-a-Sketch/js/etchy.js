4c9f69e376487fdcfe030620bd1d1652c9ede876
Dan Davis <dmedia.dan@gmail.com>
2015-10-15 16:37:32 -0400

    got the grid working

---
+++ Etch-a-Sketch/js/etchy.js
@@ -0,0 +1,17 @@
+etchySize = 64;
+
+$(document).ready(function(){
+      for(x=0; x<9;x++) {
+          var grid = document.createElement('div');
+          grid.className = "grid-squares";
+
+    document.getElementById('grid').appendChild(grid);
+}
+
+
+
+      // var divCreate = $("<div></div>");
+      // $("#etchy-grid").(divCreate);
+      //
+      // $('#etchy-grid > div').addClass("grid-squares");
+});
