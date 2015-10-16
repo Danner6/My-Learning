fb56ff69b820844f2ce7f63d6589ec80f8bb401f
Dan Davis <dmedia.dan@gmail.com>
2015-10-15 22:38:31 -0400

    changed grid to work with my own written code.

--- Etch-a-Sketch/js/etchy.js
+++ Etch-a-Sketch/js/etchy.js
@@ -1,17 +1,22 @@
 etchySize = 64;
 
 $(document).ready(function(){
-      for(x=0; x<9;x++) {
-          var grid = document.createElement('div');
-          grid.className = "grid-squares";
+      for(x=0; x<etchySize;x++) {
+            var gridSquares = $( "<div class='grid-squares'/></div>" )
+            $( gridSquares ).appendTo("#grid");
 
-    document.getElementById('grid').appendChild(grid);
-}
-
+      }
 
 
-      // var divCreate = $("<div></div>");
-      // $("#etchy-grid").(divCreate);
-      //
-      // $('#etchy-grid > div').addClass("grid-squares");
+    $('.grid-squares').on('mouseenter', function(){
+          $(this).addClass('highlight');
+   })
+
+
 });
+
+
+// var grid = document.createElement('div');
+// grid.className = "grid-squares";
+//
+// document.getElementById('grid').appendChild(grid);
