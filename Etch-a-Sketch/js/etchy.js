8eb17bb6f9ef861f8ca42b549bce935213b9b680
Dan Davis <dmedia.dan@gmail.com>
2016-02-04 22:19:12 -0500

    final commit to make everything work

--- Etch-a-Sketch/js/etchy.js
+++ Etch-a-Sketch/js/etchy.js
@@ -1,21 +1,43 @@
-etchySize = 64;
+
 
 $(document).ready(function(){
-      for(x=0; x<etchySize;x++) {
-            var gridSquares = $( "<div class='grid-squares'/></div>" )
-            $( gridSquares ).appendTo("#grid");
+      for(var x=0; x < 256; x++) {
+            $( '#container' ).append("<div></div>");
 
       }
 
-
-    $('.grid-squares').on('mouseenter', function(){
-          $(this).addClass('highlight');
-   })
+      $('div div').on('mouseenter', function(){
+            $(this).addClass('highlight');
+      })
 
 
 });
 
+function newGrid(){
+		var gridSize = prompt('Enter new grid size');
+		$('#container').empty();
 
+		for(var i=0; i < gridSize * gridSize; i++){
+			$('#container').append('<div></div>');
+		}
+
+		var newWH = (600 / gridSize);
+
+		$('div div').css('width', newWH);
+		$('div div').css('height', newWH);
+
+
+
+		$('div div').on('mouseenter', function(){
+			var r = Math.floor((Math.random() * 254) + 1);
+			var g = Math.floor((Math.random() * 254) + 1);
+			var b = Math.floor((Math.random() * 254) + 1);
+			$(this).css('background-color', 'rgb(' + r + ',' + g + ',' + b + ')');
+                  // $(this).addClass('highlight');
+
+		});
+
+	}
 // var grid = document.createElement('div');
 // grid.className = "grid-squares";
 //
